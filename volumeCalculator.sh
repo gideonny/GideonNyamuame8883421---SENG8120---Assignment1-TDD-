1946b54289ef9f992f0806ec293cc1ba5294a0fa
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-05-23 18:31:05 -0400

    added code to make test for empty values pass

--- volumeCalculator.sh
+++ volumeCalculator.sh
@@ -6,11 +6,19 @@ calculate_volume() {
     local width=$2
     local height=$3
 
+     # Check for empty input
+    if [[ -z "$length" || -z "$width" || -z "$height" ]]; then
+        echo "Error: At least one dimension is empty."
+        return 1
+    fi
+
+    #check if inputs are negative
     if ((length < 0 || width < 0 || height < 0)); then
         echo "Error: Dimensions must be positive numbers."
         return 1
     fi
 
+    #check if inputs are zero
     if ((length == 0 || width == 0 || height == 0)); then
         echo "Error: At least one dimension is zero. Shape is no longer a 3D shape therefore volume is zero."
         return 1
