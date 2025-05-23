cad09fd6ad181667be930f567999d39cf9b8c97f
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-05-23 17:01:22 -0400

    refactored code to make test for negative values pass

--- volumeCalculator.sh
+++ volumeCalculator.sh
@@ -2,5 +2,13 @@
 
 #Function to calculate volume of a rectangular prism
 calculate_volume() {
+    local length=$1
+    local width=$2
+    local height=$3
+
+    if ((length < 0 || width < 0 || height < 0)); then
+        echo "Error: Dimensions must be positive numbers."
+        return 1
+    fi
     echo $(( $1 * $2 * $3 ))
 }
